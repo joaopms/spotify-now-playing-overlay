6479bd3acb30747746dc41a52c3a88f862e187fa
João Silva <git@joaopms.pt>
2024-09-25 23:07:19 +0100

    Prepend the GitHub pages path to the assets URLs

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: process.env.GITHUB_ACTIONS == "true"
+    ? "/spotify-now-playing-overlay/"
+    : "/"
 })
